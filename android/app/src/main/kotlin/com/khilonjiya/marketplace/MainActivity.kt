d2816746c0c5221d4a187eafcaa9f92293765b0f
bhabajit54321 <bhabajitventures@gmail.com>
2025-07-11 02:10:02 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/khilonjiya/marketplace/MainActivity.kt
+++ android/app/src/main/kotlin/com/khilonjiya/marketplace/MainActivity.kt
@@ -3,4 +3,6 @@ package com.khilonjiya.marketplace
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    // No additional configuration needed for Flutter v2 embedding
+    // Flutter 3.24+ handles plugin registration automatically
 }
