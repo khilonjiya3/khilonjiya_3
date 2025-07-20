74213abe62458100977c636ac55cc9b16d3684c0
khilonjiya2 <pankajventures123@gmail.com>
2025-07-20 21:54:51 +0530

    Update search_page.dart

--- lib/presentation/home_marketplace_feed/search_page.dart
+++ lib/presentation/home_marketplace_feed/search_page.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 import 'package:flutter_google_places_hoc081098/flutter_google_places_hoc081098.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import './widgets/square_product_card.dart';
 import './widgets/shimmer_widgets.dart';
