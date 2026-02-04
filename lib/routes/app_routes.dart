9fb99e468aea617283723dcbc4e79bf7c1eb6aca
khilonjiya3 <pankajdoleyapdcl@gmail.com>
2026-02-04 20:51:06 +0530

    Update app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,74 +1,23 @@
 import 'package:flutter/material.dart';
 
-// ROLE SELECTION
 import '../presentation/role_selection/role_selection_screen.dart';
-
-// LOGIN SCREENS
-import '../presentation/login_screen/job_seeker_login_screen.dart';
-import '../presentation/login_screen/employer_login_screen.dart';
-
-// JOB SEEKER
+import '../presentation/login/job_seeker_login_screen.dart';
+import '../presentation/login/employer_login_screen.dart';
 import '../presentation/home_marketplace_feed/home_jobs_feed.dart';
-
-// EMPLOYER
 import '../presentation/company/dashboard/company_dashboard.dart';
 
 class AppRoutes {
-  /* ------------------------------------------------------------
-   ROUTE NAMES
-  ------------------------------------------------------------- */
+  static const roleSelection = '/';
+  static const jobSeekerLogin = '/job-seeker-login';
+  static const employerLogin = '/employer-login';
+  static const homeJobsFeed = '/home-jobs-feed';
+  static const companyDashboard = '/company-dashboard';
 
-  // ENTRY
-  static const String initial = '/';
-
-  // ROLE
-  static const String roleSelection = '/role-selection';
-
-  // LOGIN
-  static const String jobSeekerLogin = '/job-seeker-login';
-  static const String employerLogin = '/employer-login';
-
-  // DASHBOARDS
-  static const String homeJobsFeed = '/home-jobs-feed';
-  static const String companyDashboard = '/company-dashboard';
-
-  /* ------------------------------------------------------------
-   ROUTE MAP
-  ------------------------------------------------------------- */
-
-  static final Map<String, WidgetBuilder> routes = {
-    // ENTRY POINT
-    initial: (_) => const RoleSelectionScreen(),
-
-    // ROLE
+  static final routes = <String, WidgetBuilder>{
     roleSelection: (_) => const RoleSelectionScreen(),
-
-    // LOGIN
     jobSeekerLogin: (_) => const JobSeekerLoginScreen(),
     employerLogin: (_) => const EmployerLoginScreen(),
-
-    // DASHBOARDS
     homeJobsFeed: (_) => const HomeJobsFeed(),
     companyDashboard: (_) => const CompanyDashboard(),
   };
-
-  /* ------------------------------------------------------------
-   HELPERS
-  ------------------------------------------------------------- */
-
-  static Future<void> go(
-    BuildContext context,
-    String route, {
-    bool replace = false,
-  }) async {
-    if (replace) {
-      await Navigator.pushReplacementNamed(context, route);
-    } else {
-      await Navigator.pushNamed(context, route);
-    }
-  }
-
-  static void back(BuildContext context) {
-    Navigator.pop(context);
-  }
 }
