3f0b1cdd3b36ededd40d4c19bf1e990299da0115
khilonjiya3 <pankajdoleyapdcl@gmail.com>
2026-02-11 11:30:00 +0530

    Update section_header.dart

--- lib/presentation/home_marketplace_feed/widgets/home_sections/section_header.dart
+++ lib/presentation/home_marketplace_feed/widgets/home_sections/section_header.dart
@@ -3,41 +3,31 @@ import '../../../../core/ui/khilonjiya_ui.dart';
 
 class SectionHeader extends StatelessWidget {
   final String title;
-  final String actionText;
-  final VoidCallback? onAction;
+  final String ctaText;
+  final VoidCallback? onTap;
 
   const SectionHeader({
     Key? key,
     required this.title,
-    this.actionText = "View all",
-    this.onAction,
+    required this.ctaText,
+    this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 18, 16, 10),
-      child: Row(
-        children: [
-          Expanded(
-            child: Text(
-              title,
-              style: KhilonjiyaUI.h2.copyWith(fontWeight: FontWeight.w800),
-            ),
+    return Row(
+      children: [
+        Expanded(
+          child: Text(
+            title,
+            style: KhilonjiyaUI.hTitle.copyWith(fontWeight: FontWeight.w900),
           ),
-          InkWell(
-            borderRadius: BorderRadius.circular(12),
-            onTap: onAction,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-              child: Text(
-                actionText,
-                style: KhilonjiyaUI.link,
-              ),
-            ),
-          ),
-        ],
-      ),
+        ),
+        InkWell(
+          onTap: onTap,
+          child: Text(ctaText, style: KhilonjiyaUI.link),
+        ),
+      ],
     );
   }
 }
