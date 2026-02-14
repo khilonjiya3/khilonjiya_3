cfa0eae41babb79449d8cca7684a212d3584b72c
khilonjiya3 <pankajdoleyapdcl@gmail.com>
2026-02-14 20:55:27 +0530

    Update job_seeker_main.shell.dart

--- lib/presentation/home_marketplace_feed/job_seeker_main.shell.dart
+++ lib/presentation/home_marketplace_feed/job_seeker_main.shell.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-import '../../common/widgets/khilonjiya_bottom_nav.dart';
+import '../common/widgets/khilonjiya_bottom_nav.dart';
 
 import 'home_jobs_feed.dart';
+import 'my_jobs_page.dart';
+import 'messages_page.dart';
 import 'saved_jobs_page.dart';
-import 'recommended_jobs_page.dart';
-import 'profile_performance_page.dart';
-import 'job_search_page.dart';
+import 'profile_page.dart';
 
 class JobSeekerMainShell extends StatefulWidget {
   const JobSeekerMainShell({Key? key}) : super(key: key);
@@ -18,15 +18,13 @@ class JobSeekerMainShell extends StatefulWidget {
 class _JobSeekerMainShellState extends State<JobSeekerMainShell> {
   int _index = 0;
 
-  // IMPORTANT:
-  // We keep pages in memory so it feels fast and world-class.
-  // Home feed won't reload every time you switch tabs.
+  // Keep pages alive (world-class UX)
   late final List<Widget> _pages = [
     const HomeJobsFeed(), // Home
-    const JobSearchPage(), // My Jobs (for now using your job_search_page.dart)
-    const _MessagesPlaceholderPage(), // Messages (blank for now)
+    const MyJobsPage(), // My Jobs
+    const MessagesPage(), // Messages
     const SavedJobsPage(), // Saved
-    const ProfilePerformancePage(), // Profile (read-only style page)
+    const ProfilePage(), // Profile
   ];
 
   void _onTap(int i) {
@@ -44,27 +42,6 @@ class _JobSeekerMainShellState extends State<JobSeekerMainShell> {
       bottomNavigationBar: KhilonjiyaBottomNav(
         currentIndex: _index,
         onTap: _onTap,
-      ),
-    );
-  }
-}
-
-// ------------------------------------------------------------
-// TEMP MESSAGES PAGE (BLANK)
-// ------------------------------------------------------------
-class _MessagesPlaceholderPage extends StatelessWidget {
-  const _MessagesPlaceholderPage();
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text(
-            "Messages will be added here later",
-            textAlign: TextAlign.center,
-          ),
-        ),
       ),
     );
   }
